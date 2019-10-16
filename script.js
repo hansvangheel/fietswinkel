b4484f44ff1a1c571d15e1c5bbf7a43983e48b17
intergalactic20 <55386536+intergalactic20@users.noreply.github.com>
2019-10-16 14:57:36 +0200

    1610_15u

--- script.js
+++ script.js
@@ -1,5 +1,69 @@
 $(document).ready(function () {
-    $(".knop").on("click", function () {
-        $("p").toggle();
-    });
+
+  // reageer op change event op radio button
+  $("[name='fietstype']").on("change", function () {
+    var checked = $("[name='fietstype']:checked").val();
+    console.log(checked);
+
+    if (checked == "Stadsfiets dames") {
+      alert('true');
+      $("[for='Maat 43']").hide();
+      $("#Maat43").hide();
+
+      $("[for='Maat 61']").hide();
+      $("#Maat61").hide();
+
+      $("[for='Geen maat']").hide();
+      $("#Geenmaat").hide();
+
+      $("[for='Maat 50']").show();
+      $("#Maat50").show();
+
+      $("[for='Maat 57']").show();
+      $("#Maat57").show();
+
+    }
+
+    if (checked == "Stadsfiets heren") {
+      alert('true');
+      $("[for='Maat 43']").hide();
+      $("#Maat43").hide();
+
+      $("[for='Maat 50']").show();
+      $("#Maat50").show();
+
+      $("[for='Maat 57']").show();
+      $("#Maat57").show();
+
+      $("[for='Maat 61']").show();
+      $("#Maat61").show();
+
+      $("[for='Geen maat']").hide();
+      $("#Geenmaat").hide();
+    }
+
+    if (checked == "Elektrische fiets dames") {
+      alert('true');
+      $("[for='Maat 43']").hide();
+      $("#Maat43").hide();
+
+      $("[for='Maat 50']").show();
+      $("#Maat50").show();
+
+      $("[for='Maat 57']").show();
+      $("#Maat57").show();
+
+      $("[for='Maat 61']").hide();
+      $("#Maat61").hide();
+
+      $("[for='Geen maat']").hide();
+      $("#Geenmaat").hide();
+    }
+
+
+
+  })
+
+
+
 });
